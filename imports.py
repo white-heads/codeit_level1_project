3645f54aedc9ea40b40e7b17eca04e9eaa221225
yeonho <gogo4924@naver.com>
2025-05-23 14:55:30 +0900

    import.py 파일 추가

---
+++ imports.py
@@ -0,0 +1,31 @@
+﻿# 표준 라이브러리
+import os
+import time
+import json
+import random
+import collections
+
+# 서드파티 라이브러리
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+
+# PyTorch 관련
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
+import torchmetrics
+
+# 학습 관련 도구
+import albumentations as A
+import albumentations.pytorch
+from pycocotools.coco import COCO
+
+# Scikit-learn
+from sklearn.model_selection import train_test_split
+
+# 설정 파일
+from omegaconf import OmegaConf
